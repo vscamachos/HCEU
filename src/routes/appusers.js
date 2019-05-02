e7fb0afcce92da4ff47e093f638c6ffa821022f6
marjuliam <mjmartinezv28@gmail.com>
2019-05-02 15:53:22 -0500

    Commi tUserVerificado

--- src/routes/appusers.js
+++ src/routes/appusers.js
@@ -3,31 +3,57 @@ const router = express.Router();
 
 const User = require('../models/user');
 
-
+//obteniendo la lista de la base
 router.get('/users', async (req,res)=>{
     const user = await User.find();
     res.json(user); 
 });
 
+//busca por id
 router.get('/users/:id',async (req, res)=>{
     const user = await User.findById(req.params.id);
     res.json(user);
 });
 
+//crea usuario
 router.post('/users',async (req, res)=>{
-    const { userName,userDescription} = req.body;
+    //los parametros que vamos a 'USAR' del request body
+        const { userName,userDocument,userDescription,      
+        userBirthday,userRol,userTelphone,userAdress,
+        userCountry,userEmail} = req.body;
+    //nuevo objeto con los parametros pedidos    
     const newUser = {
         userName,
+        userDocument,
         userDescription,      
+        userBirthday,
+        userRol,
+        userTelphone,
+        userAdress,
+        userCountry,
+        userEmail
     };
     const user = new User(newUser);
     await user.save();
     res.json({status: 'User Saved'});
 });
 
+//actualizar
 router.put('/users/:id',async (req, res)=>{
-    const { userName, userDescription } = req.body;
-    const newUser = { userName, userDescription };
+    const { userName,userDocument,userDescription,      
+        userBirthday,userRol,userTelphone,userAdress,
+        userCountry,userEmail} = req.body;
+    const newUser = {
+        userName,
+        userDocument,
+        userDescription,      
+        userBirthday,
+        userRol,
+        userTelphone,
+        userAdress,
+        userCountry,
+        userEmail
+    };
     await User.findByIdAndUpdate(req.params.id, newUser);
     res.json({status: 'User Updated'});
 });
