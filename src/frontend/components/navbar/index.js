e584f42ae97a629a5884bd07964c2c50039eaa30
Nicolai Romero <anromerom@unal.edu.co>
2019-05-04 21:30:40 -0500

    Añadido el logo del proyecto en la navbar.

--- src/frontend/components/navbar/index.js
+++ src/frontend/components/navbar/index.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import hceuicono from './hceuicono.png';
+import hceuicono from './LogoApp.svg';
 import './index.css';
 // data
 
@@ -25,8 +25,8 @@ class Navbar extends Component{
 
         <nav className="navbar Bcolor1">
         <a className="navbar-brand" id="titledoctor" href="/">
-        <img src={hceuicono} width="48" height="48" id="hceuicono" className="d-inline-block align-top" alt={"Tenkuru icono"} />
-        HCEU</a>
+        <img src={hceuicono} width="150px" id="hceuicono" className="d-inline-block align-top" alt={"Tenkuru icono"} />
+        </a>
         <div className="col-6"></div>
         <ul className="navbar-nav bd-navbar-nav flex-row-reverse">
         <li className="nav-item">
@@ -38,7 +38,7 @@ class Navbar extends Component{
         <ul className="navbar-nav bd-navbar-nav flex-row-reverse" id="titledoctor"> <h1>{this.state.name} </h1>
         </ul>
 
-        <ul className="navbar-nav bd-navbar-nav flex-row-reverse" id="titledoctor"> <h3>{this.state.identification} </h3>
+        <ul className="navbar-nav bd-navbar-nav flex-row-reverse" id="titledoctor">  <h3>c.c.{this.state.identification} </h3>
         </ul>
 
 
