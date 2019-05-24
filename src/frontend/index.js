fd38cb7e35fb8cde2a2edfddac397afa3f92c6bc
Sebastian Camacho <vscamachos@unal.edu.co>
2019-05-23 22:34:45 -0500

    revision 1

--- src/frontend/index.js
+++ src/frontend/index.js
@@ -1,8 +1,12 @@
 import React from 'react';
-import { render } from 'react-dom';
+import ReactDOM from 'react-dom';
 import 'bootstrap/dist/css/bootstrap.css';
 import './index.css';
 import App from './App'
 
 
-render(<App/>, document.getElementById('app'));
+
+ReactDOM.render(
+<App />,
+document.getElementById('root')
+);
