5aa4b7183629c28e7f33787484dbca93b3534274
Sebastian Camacho <vscamachos@unal.edu.co>
2019-06-04 15:52:29 -0500

    m

--- src/frontend/App.js
+++ src/frontend/App.js
@@ -4,24 +4,24 @@ import AppNavbar from './components/navbar/index.js';
 import Form from './components/form/index.js';
 
 
-class App extends Component{    
+class App extends Component{
 
     render(){
         return (
             <div>
               <div className="">
-                <AppNavbar/>
+                //<AppNavbar/>
               </div>
-              
+
               <div className="container Bcolor2">
-                <Login/>
+                //<Login/>
               </div>
 
               <div>
-                <Form/>
-              </div>                      
-            </div>            
+                //<Form/>
+              </div>
+            </div>
         )
     }
-}   
+}
 export default App;
